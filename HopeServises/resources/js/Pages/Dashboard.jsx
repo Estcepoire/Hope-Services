e262be8efb7ba79fe25fdaae64fc829e22d25faa
Mamison Rajaoharisaona <narymams@gmail.com>
2024-04-25 16:03:41 +0300

    React

--- HopeServises/resources/js/Pages/Dashboard.jsx
+++ HopeServises/resources/js/Pages/Dashboard.jsx
@@ -3,10 +3,11 @@ import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
+        // <AuthenticatedLayout
+            // user={auth.user}
+            // header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        // >
+        <>
             <Head title="Dashboard" />
 
             <div className="py-12">
@@ -16,6 +17,7 @@ export default function Dashboard({ auth }) {
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        
+        </>
     );
 }
